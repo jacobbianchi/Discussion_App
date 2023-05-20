41847ab3472dfb437be231528019fe6015b10c79
Jacob <jacobbia@comcast.net>
2023-05-19 18:34:10 -0700

    added bar plot and density curve

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from matplotlib import pyplot as plt
 import io
+import seaborn as sns
 
 st.write('''# Explore a Dataset!''')
 st.sidebar.write('''# Options''')
@@ -14,8 +15,7 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
 
     # If the user checks the box, display the DataFrame
-    show_df = st.checkbox("Show DataFrame")
-    if show_df:
+    if st.checkbox("Show DataFrame"):
         st.dataframe(df)
 
     # Determine the numeric/categorical/boolean/date-time columns
@@ -54,29 +54,61 @@ if uploaded_file is not None:
 
             # Histogram Display
             st.write('''### Histogram''')
-            color_selection = st.color_picker("Color", "#2774AE")
             hist_bins = st.slider("Number of Bins", min_value = 5, max_value = 150, value = 30)
+            bin_color_selection = st.color_picker("Bin Color", "#2774AE")
+            show_density = st.checkbox("Density Curve")
+            if show_density:
+                density_color_selection = st.color_picker("Density Curve Color", "#FFD100")
             hist_title = st.text_input("Set Title", "Histogram")
             hist_x_title = st.text_input("Set x-axis Title", col_selection)
             hist_y_title = st.text_input("Set y-axis Title", "Count")
             fig, ax = plt.subplots()
-            ax.hist(df[col_selection], bins = hist_bins, edgecolor = "black", color = color_selection)
+            ax.hist(df[col_selection], bins = hist_bins, edgecolor = "black", color = bin_color_selection)
             ax.set_title(hist_title)
             ax.set_xlabel(hist_x_title)
             ax.set_ylabel(hist_y_title)
+            if show_density:
+                density = sns.kdeplot(df[col_selection], color = density_color_selection, ax = ax.twinx())
+                density.set_yticks([])
             st.pyplot(fig)
             
             # Download the histogram
             plot_bytes = io.BytesIO()
-            fig.savefig(plot_bytes, format='png', dpi = 300)
+            fig.savefig(plot_bytes, format='png', dpi = 300, bbox_inches = "tight")
             plot_bytes.seek(0)
-            st.download_button(label='Download Plot', data=plot_bytes, file_name='plot.png', mime='image/png')
+            st.download_button(label='Download Plot', data=plot_bytes, file_name='histogram.png', mime='image/png')
         
         # What to do if it is a categorical variable
         if col_type_selection == "Categorical":
 
+            # Preparing the Data
+            props = pd.DataFrame(df[col_selection].value_counts(normalize = True).reset_index())
+
             # Table of Proprtions
             st.write('''### Table of Proportions:''')
-            st.table(df[col_selection].value_counts(normalize = True))
+            st.table(props)
 
-            # Bar Graph Display
+            # Bar Plot Display
+            st.write('''### Bar Plot''')
+            sort_bars = st.radio("Sort:", ("Ascending", "Descending"), index = 0)
+            if sort_bars == "Ascending":
+                props = props.sort_values(by = "proportion", ascending = True)
+            else:
+                props = props.sort_values(by = "proportion", ascending = False)
+            bar_color_selection = st.color_picker("Color", "#2774AE")
+            bar_title = st.text_input("Set Title", "Bar Plot")
+            bar_x_title = st.text_input("Set x-axis Title", col_selection)
+            bar_y_title = st.text_input("Set y-axis Title", "Proportion")
+            fig, ax = plt.subplots()
+            ax.bar(props[col_selection], props["proportion"], color = bar_color_selection)
+            ax.set_title(bar_title)
+            ax.set_xlabel(bar_x_title)
+            ax.set_ylabel(bar_y_title)
+            ax.set_xticklabels(props[col_selection], rotation = 45)
+            st.pyplot(fig)
+
+            # Download the bar plot
+            plot_bytes = io.BytesIO()
+            fig.savefig(plot_bytes, format='png', dpi = 300, bbox_inches = "tight")
+            plot_bytes.seek(0)
+            st.download_button(label='Download Plot', data=plot_bytes, file_name='bar_plot.png', mime='image/png')
